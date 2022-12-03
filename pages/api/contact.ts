1989096ed7f58e97826d5368577697817fd570e5
Alaaddine TABET <alta.tabet@gmail.com>
2022-12-03 22:02:49 +0100

    add the contact api

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,3 +1,34 @@
-export default function hundler(req :any, res: any) {
-    console.log("my body",req.body)
+
+export default function hundler(req: any, res: any) {
+
+    //require('dotenv').config()
+
+    //const PASSWORD = process.env.password
+    let nodemailer = require('nodemailer')
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
+        auth: {
+            user: 'tabet.sendmail@gmail.com',
+            pass: 'ikwyqbpwjlwynotz',
+        },
+        secure: true,
+    })
+
+    const mailData = {
+        from: 'tabet.sendmaill@gmail.com',
+        to: 'alae.tabet02@gmail.com',
+        subject: `Message From ${req.body.name}`,
+        text: req.body.message + " | Sent from: " + req.body.email,
+        html: `<div>${req.body.message}</div><p>Sent from:
+        ${req.body.email}</p>`
+      }
+      transporter.sendMail(mailData, function (err : any, info : any) {
+        if(err)
+          console.log(err)
+        else
+          console.log(info)
+      })
+      res.status(200)
+
 }
